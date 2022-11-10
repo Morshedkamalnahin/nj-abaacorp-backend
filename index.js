59aa5f8e640a32ca1076a5cf85833b2339bab198
NAHIN <mknahin2000@gmail.com>
2022-11-10 12:52:11 +0600

    added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
@@ -22,20 +22,52 @@ async function run() {
     try {
         await client.connect();
         const database = client.db("njabaacorp");
-        const Collection = database.collection("blog");
+        const blogsCollection = database.collection("blog");
+        const instagramCollection = database.collection('instagram');
+        const projectCollection = database.collection('project')
+        const sliderCollection = database.collection('slider')
         console.log('connected db')
+
+        //get api
         app.get('/add_blog', async (req, res) => {
 
-            const blogs = Collection.find({});
+            const blogs = blogsCollection.find({});
             const blog = await blogs.toArray();
             res.send(blog)
 
         })
+        app.get('/instagram', async (req, res) => {
 
+            const instagrams = instagramCollection.find({});
+            const instagram = await instagrams.toArray();
+            res.send(instagram)
 
+        })
+        app.get('/project', async (req, res) => {
+
+            const projects = projectCollection.find({});
+            const project = await projects.toArray();
+            res.send(project)
+
+        })
+        app.get('/slider', async (req, res) => {
+
+            const sliders = sliderCollection.find({});
+            const slider = await sliders.toArray();
+            res.send(slider)
+
+        })
+        //// find single data
+        app.get('/add_blog/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const blog = await blogsCollection.findOne(query);
+            res.json(blog);
+        })
+
+        //post api
         app.post('/add_blog', async (req, res) => {
-            console.log(req.body)
-            console.log(req.files)
+
 
 
             const { title, dec, date } = req.body;
@@ -46,13 +78,98 @@ async function run() {
 
             const newBlog = { title, dec, date, img: imgBuffer }
 
-            const result = await Collection.insertOne(newBlog);
+            const result = await blogsCollection.insertOne(newBlog);
             res.json({ success: true })
 
 
             console.log('hitt post api', result.insertedId)
             // res.send(result)
 
+        })
+        app.post('/instagram', async (req, res) => {
+
+
+
+            const { title, link } = req.body;
+            const pic = req.files.img;
+            const picData = pic.data;
+            const encodePic = picData.toString('base64');
+            const imgBuffer = Buffer.from(encodePic, 'base64')
+
+            const newInstagramCard = { title, link, img: imgBuffer }
+
+            const result = await instagramCollection.insertOne(newInstagramCard);
+            res.json({ success: true })
+
+
+            console.log('hitt post api', result.insertedId)
+            // res.send(result)
+
+        })
+        app.post('/project', async (req, res) => {
+
+
+            const { title, link } = req.body;
+            const pic = req.files.img;
+            const picData = pic.data;
+            const encodePic = picData.toString('base64');
+            const imgBuffer = Buffer.from(encodePic, 'base64')
+
+            const newProject = { title, link, img: imgBuffer }
+
+            const result = await projectCollection.insertOne(newProject);
+            res.json({ success: true })
+
+
+            console.log('hitt post api', result.insertedId)
+            // res.send(result)
+
+        })
+        app.post('/slider', async (req, res) => {
+
+
+            const { title, dec } = req.body;
+            const pic = req.files.img;
+            const picData = pic.data;
+            const encodePic = picData.toString('base64');
+            const imgBuffer = Buffer.from(encodePic, 'base64')
+
+            const newSlider = { title, dec, img: imgBuffer }
+
+            const result = await sliderCollection.insertOne(newSlider);
+            res.json({ success: true })
+
+
+            console.log('hitt post api', result.insertedId)
+            // res.send(result)
+
+        })
+
+
+        //delete api
+        app.delete('/blog_delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await blogsCollection.deleteOne(query);
+            console.log(result)
+        })
+        app.delete('/instagram_delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await instagramCollection.deleteOne(query);
+            console.log(result)
+        })
+        app.delete('/project_delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await projectCollection.deleteOne(query);
+            console.log(result)
+        })
+        app.delete('/slider_delete/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await sliderCollection.deleteOne(query);
+            console.log(result)
         })
     } finally {
         // await client.close();
